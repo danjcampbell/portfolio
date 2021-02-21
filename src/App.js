5872a3e05c3075d305ba14cb163b260a54e1b21e
Danny Campbell <dannycampbell726@gmail.com>
2021-02-21 12:24:49 -0500

    linkedin changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import dan from "./dan1.JPG";
+import "./App.css";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://platform.linkedin.com/badges/js/profile.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className=" -header">
+        <div
+          className="LI-profile-badge"
+          data-version="v1"
+          data-size="medium"
+          data-locale="en_US"
+          data-type="vertical"
+          data-theme="dark"
+          data-vanity="danieljcampbell1"
         >
-          Learn React
-        </a>
+          <a
+            className="LI-simple-link"
+            href="https://www.linkedin.com/in/danieljcampbell1?trk=profile-badge"
+          >
+            Daniel Campbell
+          </a>
+        </div>
+        <p className="paragraph">DanJCampbell.com is under Construction.</p>
       </header>
     </div>
   );
