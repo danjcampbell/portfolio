18c93df0d911bcb46f2f9844ecc85f8f5a6e6c01
Daniel Campbell <dannycampbell726@gmail.com>
2021-07-20 17:26:31 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
         <Route path="/resume" render={() => <Resume />} />
         <Route path="/3dModel" render={() => <ThreeDModel />} />
       </Router>
+<input type="file" />
     </div>
   );
 }
